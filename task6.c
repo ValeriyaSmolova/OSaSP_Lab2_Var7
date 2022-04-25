8f07e559e1cf9fd3552edcf0f650591d151ddfa1
ValeriyaSmolova <100532617+ValeriyaSmolova@users.noreply.github.com>
2022-04-25 13:04:12 +0300

    Update task6.c

--- task6.c
+++ task6.c
@@ -2,55 +2,56 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <errno.h>
+#include <limits.h>
 
 void output_data_dir(char *dir)
 {
+		
+	DIR *StreamDir;
+	
+	if((StreamDir = opendir(dir)) == NULL)
+	{
+		perror("opendir");
+		return;
+	}
 
-  DIR *StreamDir;
+	errno = 0;
+	struct dirent *Data;
 
-  if((StreamDir = opendir(dir)) == NULL)
-  {
-    perror("opendir");
-    return;
-  }
+	while((Data = readdir(StreamDir)) != NULL)
+	{
+		printf("%s\n", Data->d_name);
+	}
 
-  errno = 0;
-  struct dirent *Data;
+	if((Data = readdir(StreamDir)) == NULL && errno != 0)
+	{
+		perror("readdir");
+		return;
+	}
 
-  while((Data = readdir(StreamDir)) != NULL)
-  {
-    printf("%s\n", Data->d_name);
-  }
-
-  if((Data = readdir(StreamDir)) == NULL && errno != 0)
-  {
-    perror("readdir");
-    return;
-  }
-
-  if(closedir(StreamDir) != 0)
-  {
-    perror("closedir");
-    return;
-  }
-
+	if(closedir(StreamDir) != 0)
+	{
+		perror("closedir");
+		return;
+	}
+	
 }
 
 int main(int argc, char *argv[])
 {
-  char current_dir[256];
+	char current_dir[PATH_MAX];
+	
+	if(getcwd(current_dir, PATH_MAX) == NULL)
+	{
+		perror("getcwd");
+		return 1;
+	}
 
-  if(getcwd(current_dir, 256) == NULL)
-  {
-    perror("getcwd");
-    return 1;
-  }
-
-  printf("Current dir:\n");
-  output_data_dir(current_dir);
-  printf("-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.\n");
-  printf("Root dir:\n");
-  output_data_dir("/");
-
-  return 0;
+	printf("Current dir:\n");
+	output_data_dir(current_dir);
+	printf("•••• −−− •−• −−− −−−− • −−• −−−   •−− •− −−   −•• −• •−•− −−••−− \n");
+	printf("Root dir:\n");
+	output_data_dir("/");
+	
+	return 0;
 }
